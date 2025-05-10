b39b97e1e70af6f65428ce74bb5eacd057f132c9
dan-reds <drojash@unal.edu.co>
2025-05-10 09:45:47 -0500

    DuringClass

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,18 @@
+## wordcount.py
 
+from homework.src._internals.parse_args import parse_args
+
+def main():
+    """
+    Main function to count words in a file.
+    """
+
+    input_folder, output_folder = parse_args()
+
+    print(f"Processing files from {input_folder} to {output_folder}")
+
+
+if __name__ == "__main__":
+    main()
+    
+    
